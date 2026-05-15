f497ac3f1abcd66ac06b412bd7a93aa512816d84
Vítor Camboim <vitorcamboimsim@gmail.com>
2026-05-15 13:33:35 -0300

    Entrega 2

--- rdocs/entrega2.R
+++ rdocs/entrega2.R
@@ -22,3 +22,60 @@ source("rdocs/source/packages.R")
 # de teste depreciados, ou ao menos deixando como comentário. Dê preferência
 # as funções dos pacotes contidos no Tidyverse para realizar suas análises.
 # ---------------------------------------------------------------------------- #
+
+
+# Atribuindo os bancos as variáveis
+jogos <- read_excel("C:/Users/v4746/OneDrive/Desktop/ESTAT/Projeto-Fantasma-2026.1/banco/pixel110011.xlsx", sheet = "info_jogos")
+compras <- read_excel("C:/Users/v4746/OneDrive/Desktop/ESTAT/Projeto-Fantasma-2026.1/banco/pixel110011.xlsx", sheet = "info_compras")
+
+# Organizando nome dos ID's no banco de dados
+jogos <- jogos %>%
+  rename(game_id = ga8e_id)
+
+compras <- compras %>%
+  rename(purchase_id = purch4se_id, 
+         player_id = p7ayer_id, 
+         game_id = gam5_id, 
+         product_id = pr0duct_id)
+
+# Juntando os bancos de dados
+
+jogos_compras <- left_join(jogos, compras, by = "game_id")
+
+# Criando BD apenas com compras de 2025 do jogo "Minecaft"
+
+minecraft_2025 <- jogos_compras %>%
+  filter(jogo == "Minecraft", year(data_compra) == 2025)
+
+# Agrupando a soma dos valores monetários por mês
+
+receita_minecraft_2025 <- minecraft_2025 %>%
+  mutate(mes = lubridate::month(data_compra, label = TRUE)) %>%
+  group_by(mes) %>%
+  summarise(
+    total_reais = sum(quantidade * `preco_unitario_R$`),
+    total_dolares = round(total_reais / 5.19, 2))
+
+# Gráfico de linhas do total de dólares por mês:
+
+grafico_receita_2025 <- receita_minecraft_2025 %>%
+  ggplot(aes(x=mes, y=total_dolares, group=1)) +
+  geom_line(size=1,colour="#A11D21") + geom_point(colour="#A11D21",
+                                                  size=2) +
+  labs(x="Mês", y="Receita em dólares (US$)") +
+  theme_estat()
+ggsave("series_uni.pdf", width = 158, height = 93, units = "mm")
+
+# Tabela para valores por mês:
+# Aprendendo a usar knitr para automatizar o latex
+
+library(knitr)
+
+receita_minecraft_2025 %>%
+  kable(
+    format = "latex",
+    booktabs = TRUE,
+    digits = 2,
+    col.names = c("Mês", "Receita (R$)", "Receita (US$)"),
+    caption = "{@tbl-receita} Receita mensal do Minecraft em 2025"
+  )
