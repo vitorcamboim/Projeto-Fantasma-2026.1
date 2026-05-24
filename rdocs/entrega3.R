9bbcea8d724d82ff8fb81101d81a987cca3b3555
Vítor Camboim <vitorcamboimsim@gmail.com>
2026-05-24 18:20:57 -0300

    Entrega análise 3

--- rdocs/entrega3.R
+++ rdocs/entrega3.R
@@ -1,3 +1,4 @@
+setwd("C:/Users/v4746/OneDrive/Desktop/ESTAT/Projeto-Fantasma-2026.1")
 source("rdocs/source/packages.R")
 
 # ---------------------------------------------------------------------------- #
@@ -22,3 +23,23 @@ source("rdocs/source/packages.R")
 # de teste depreciados, ou ao menos deixando como comentário. Dê preferência
 # as funções dos pacotes contidos no Tidyverse para realizar suas análises.
 # ---------------------------------------------------------------------------- #
+
+# Trocar para o próprio diretório
+infos_jogadores <- read_excel("C:/Users/v4746/OneDrive/Desktop/ESTAT/Projeto-Fantasma-2026.1/banco/pixel110011.xlsx", sheet = "infos_jogadores")
+
+# Todas variáveis escritas corretamente, então sem correções. Seguindo...
+
+# Foi pedido o tempo em horas e está em minutos, então transformamos...
+# Gráfico do tempo médio mensal pela idade
+grafico_idade_tempo <- infos_jogadores %>%
+  mutate(tempo_medio_mensal_horas = (logins_mes * tempo_medio_sessao_min) / 60) %>%
+  ggplot() +
+  aes(x = idade, y = tempo_medio_mensal_horas) +
+  geom_point(colour = "#A11D21", size = 3) +
+  labs(
+    x = "Idade (anos)",
+    y = "Tempo médio mensal gasto (horas)"
+    ) +
+  theme_estat()
+ggsave("disp_uni.pdf", width = 158, height = 93, units = "mm")
+
